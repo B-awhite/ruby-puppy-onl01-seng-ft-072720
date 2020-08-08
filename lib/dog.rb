a3c64e5ed049dce01455040af9211209aa9a06f3
Bria White <bria.awhite@gmail.com>
2020-08-08 23:52:24 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,18 @@
 class Dog
   
+  @@all = []
   
+  attr_accessor :name 
+  
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end 
+  
+  def seld.all
+    @@all 
+  end 
+  
+  def self.print_all 
+    
 end 
